0737685c8ae43d3ff0d7ccdca703d463b60fa527
Wanderson Honorato <wanderson.honorato1998@gmail.com>
2025-04-11 16:24:13 -0300

    feat(controller): add Controller to test endpoints

--- src/main/java/com/novaterra/SendReportEmail/controller/SendEmailManuallyController.java
+++ src/main/java/com/novaterra/SendReportEmail/controller/SendEmailManuallyController.java
@@ -1,4 +1,25 @@
-package com.novaterra.SendReportEmail.controller;
+package com.novaterra.send_report_email.controller;
 
+import com.novaterra.SendReportEmail.service.ScheduledShipmentService;
+import jakarta.mail.MessagingException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.FileNotFoundException;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/sends")
 public class SendEmailManuallyController {
+
+    private final ScheduledShipmentService scheduledService;
+
+    @GetMapping("/medications")
+    public String send() throws MessagingException, FileNotFoundException {
+        scheduledService.sendToAll();
+        return "Measurements Sent";
+    }
+
 }

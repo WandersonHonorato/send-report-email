7ffe95056b76df7259bf9e7bb46ece9cef4c5d9d
Wanderson Honorato <wanderson.honorato1998@gmail.com>
2025-04-11 09:45:22 -0300

    init project

---
+++ src/main/java/com/novaterra/SendReportEmail/SendReportEmailApplication.java
@@ -0,0 +1,13 @@
+package com.novaterra.SendReportEmail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SendReportEmailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SendReportEmailApplication.class, args);
+	}
+
+}

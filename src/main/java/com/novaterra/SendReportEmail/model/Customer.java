bf7ce844021a80da79b6aab927c097aa59736a92
Wanderson Honorato <wanderson.honorato1998@gmail.com>
2025-04-11 12:05:50 -0300

    feat(model): create Customer entity with attributes

--- src/main/java/com/novaterra/SendReportEmail/model/Customer.java
+++ src/main/java/com/novaterra/SendReportEmail/model/Customer.java
@@ -1,4 +1,25 @@
 package com.novaterra.SendReportEmail.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity(name = "Clientes")
 public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+
+    private String name;
+
+    private String email;
+
+    private String fantasyName; // utilizado para encontrar o PDF do cliente
 }
